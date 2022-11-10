2921e32f4c396939d420e39fc81d0bc7217e7824
Nerakma84 <fnk8476@gmail.com>
2022-11-10 02:15:08 -0600

    changed from email to username for logging in

--- public/js/login.js
+++ public/js/login.js
@@ -2,14 +2,14 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collects values from the login form
-    const email = document.querySelector('#email-login').value.trim();
+    const userName = document.querySelector('#user-name-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
+    if (userName && password) {
       // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ userName, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
@@ -25,14 +25,14 @@ const loginFormHandler = async (event) => {
     const signupFormHandler = async (event) => {
     event.preventDefault();
         
-    const name = document.querySelector('#name-signup').value.trim();
+    const userName = document.querySelector('#user-name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
         
-    if (name && email && password) {
+    if (userName && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ userName, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
         
